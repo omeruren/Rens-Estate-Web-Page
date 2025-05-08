b26578069a34f35a6777e32104919e4ff6f22c80
omer uren <omer.uren19@gmail.com>
2025-05-08 17:06:06 +0300

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import Userouter from './routes/user.route.js';
+import AuthRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -14,9 +15,10 @@ mongoose
     console.log(err);
   });
 const app = express();
-
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is listening on 3000");
 });
 
 app.use("/api/user", Userouter);
+app.use('/api/auth', AuthRouter);
