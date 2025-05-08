4e347d9cbf261d99571a578c840136ac9a5248ae
omer uren <omer.uren19@gmail.com>
2025-05-08 20:04:56 +0300

    create Sign In api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,6 +12,28 @@ export const signup = async (req, res, next) => {
     res
       .status(201)
       .json({ success: true, message: "User Created Succesfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const isValidUser = await User.findOne({ email });
+    if (!isValidUser) return next(errorHandler(404, "User not found!"));
+    const isValidPassword = bcryptjs.compareSync(
+      password,
+      isValidUser.password
+    );
+    if (!isValidPassword) return next(errorHandler(401, "Wrong Cridentials!"));
+    const token = jwt.sign({ id: isValidUser._id }, process.env.JWT_SECRET);
+    const {password: pass, ...rest} = isValidUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
