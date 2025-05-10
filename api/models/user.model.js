8ace9e56517595695e503fc6db167148eef64f1c
omer uren <omer.uren19@gmail.com>
2025-05-10 10:51:14 +0300

    Create and add OAuth Component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -14,7 +14,11 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-    }
+    },
+    avatar: {
+        type: String,
+        default: "https:sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png"
+    },
 },{timestamps: true});
 
 const User = mongoose.model('User',userSchema);
