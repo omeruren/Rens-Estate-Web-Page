4b2641fe0d528a2ae89979421c25e0f00809b200
omer uren <omer.uren19@gmail.com>
2025-05-14 01:01:24 +0300

    Add updateListing functionality to enable updating of listings in the API

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -27,3 +27,25 @@ import { errorHandler } from "../utils/error.js";
           next(error);
       }
     }
+
+    export const updateListing = async (req, res, next) => {
+        const listingId = Listing.findById(req.params._id);
+    
+        if (!listingId) {
+            return next(errorHandler("Listing not found"));
+        }
+    
+        if (req.user._id !== listingId.userId) {
+            return next(errorHandler(403,"You are not allowed to update this listing"));
+        }
+        try {
+            const updatedListing = await Listing.findByIdAndUpdate(
+                req.params.id,
+                { $set: req.body },
+                { new: true }
+            );
+            return res.status(200).json(updatedListing);
+        } catch (error) {
+            next(error);
+        }
+    }
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,15 @@
-import express from 'express';
-import { createListing, deleteListing} from '../controllers/listing.controller.js';
-import { verifyUserToken } from '../utils/verifyUser.js';
+import express from "express";
+import {
+  createListing,
+  deleteListing,
+  updateListing
+} from "../controllers/listing.controller.js";
+import { verifyUserToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.post("/create", verifyUserToken, createListing)
-router.delete("/delete/:id", verifyUserToken, deleteListing)
+router.post("/create", verifyUserToken, createListing);
+router.delete("/delete/:id", verifyUserToken, deleteListing);
+router.post("/update/:id", verifyUserToken, updateListing);
 
 export default router;
