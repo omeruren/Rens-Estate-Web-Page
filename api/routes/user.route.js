82ba3d524fc8c76c1f98c8bca29815c4293ad784
omer uren <omer.uren19@gmail.com>
2025-05-13 21:26:10 +0300

    Add getUserListings route to user API for retrieving user-specific listings

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { deleteUser, test, updateUser } from "../controllers/user.controller.js";
+import {
+  deleteUser,
+  getUserListings,
+  test,
+  updateUser,
+} from "../controllers/user.controller.js";
 import { verifyUserToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.get('/test',test);
-router.post("/update/:id",verifyUserToken, updateUser)
-router.delete("/delete/:id",verifyUserToken, deleteUser)
+router.get("/test", test);
+router.post("/update/:id", verifyUserToken, updateUser);
+router.delete("/delete/:id", verifyUserToken, deleteUser);
+router.get("/listings/:id", verifyUserToken, getUserListings);
 
 export default router;
